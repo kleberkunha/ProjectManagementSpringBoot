da5ec9251a9361d9119eea6b3fd7a5a27cc70571
Kleber Kunha <kleberkunha@hotmail.com>
2024-03-13 13:50:10 +0100

    Creating project

---
+++ src/main/java/pjmanagement/projectmanagement/controllers/UserController.java
@@ -0,0 +1,14 @@
+package pjmanagement.projectmanagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@Getter
+@Setter
+public class UserController {
+
+    private final here the service class;
+}
---
+++ src/main/java/pjmanagement/projectmanagement/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package pjmanagement.projectmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pjmanagement.projectmanagement.entities.UserEntity;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    List<UserEntity> findByUsername(String username);
+}
